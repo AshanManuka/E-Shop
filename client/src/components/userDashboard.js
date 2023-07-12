efb344b627ecee14b476e12e5ef2c4a275c7054a
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-07-12 23:42:49 +0530

    created some components

---
+++ client/src/components/userDashboard.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+export default class userDashboard extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      customers : []
+    }
+  }
+
+  //call lifecycleMethod
+  componentDidMount(){
+    this.getAllCustomers();
+  }
+
+
+  getAllCustomers(){
+    axios.get("http://localhost:8000/customer/getCustomer").then(res=>{
+      // if(res.data.success){}
+      this.setState({
+          customers:res.data
+      });
+      console.log(this.state.customers);
+
+    });
+  }
+
+
+  render() {
+    return (
+      //code here to display
+      <div>
+        {/* {this.state.customers.map(customer => {
+          <div>
+            <h1>Customer</h1>
+            <h5>customer.customerId</h5>
+            <h5>customer.customerName</h5>
+            <h5>customer.contactNumber</h5>
+            <h5>customer.address</h5>
+            <h5>________________________________</h5>
+          </div>
+        })} */}
+      </div>
+    )
+  }
+}
