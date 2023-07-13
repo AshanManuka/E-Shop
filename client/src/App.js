6af5728c185a659755b78ecf1d56bfe8c1137321
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-07-13 17:34:20 +0530

    install routes and used it

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,22 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {BrowserRouter,Route} from 'react-router-dom';
+import Home from './components/Home';
+
 
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      customers : []
-    }
-  }
-
-  //call lifecycleMethod
-  componentDidMount(){
-    this.getAllCustomers();
-  }
-
-
-  getAllCustomers(){
-    axios.get("http://localhost:8000/customer/getCustomer").then(res=>{
-      // if(res.data.success){}
-      this.setState({
-          customers:res.data
-      });
-      console.log(this.state.customers);
-
-    });
-  }
-
-
   render() {
     return (
-      //code here to display
-      <div>
-        {/* {this.state.customers.map(customer => {
-          <div>
-            <h1>Customer</h1>
-            <h5>customer.customerId</h5>
-            <h5>customer.customerName</h5>
-            <h5>customer.contactNumber</h5>
-            <h5>customer.address</h5>
-            <h5>________________________________</h5>
-          </div>
-        })} */}
-      </div>
+        <BrowserRouter>
+              <div className='container'>
+                <NavBar/>                
+                <Route path='/' exact Component={Home}></Route>
+                {/* <Route path='/adminForm' exact Component={AdminDashboard}></Route>
+                <Route path='/orderForm' exact Component={OrderForm}></Route> */}
+                
+
+              </div>
+        </BrowserRouter>
     )
   }
 }
--- client/src/components/userDashboard.js
+++ client/src/components/userDashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 
-export default class userDashboard extends Component {
+export default class UserDashboard extends Component {
   constructor(props){
     super(props);
 
