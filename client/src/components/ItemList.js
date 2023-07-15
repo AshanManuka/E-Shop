3a3634d27c7fb80bad20eae9f2eb5dd344222776
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-07-15 19:40:02 +0530

    created userdashboard

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './../css/itemList.css';
+
 
 export default class ItemList extends Component {
-  
-    constructor(props){
+    
+  constructor(props){
         super(props);
     
         this.state = {
@@ -33,14 +35,26 @@ export default class ItemList extends Component {
       
         <div>
         {this.state.items.map(item => (
-        <div key={item.itemCode}> {/* Added a key prop */}
-          <h1>Item</h1>
-          <h5>{item.itemCode}</h5> {/* Wrapped the variable in curly braces */}
-          <h5>{item.itemName}</h5> {/* Wrapped the variable in curly braces */}
-          <h5>{item.imageUrl}</h5> {/* Wrapped the variable in curly braces */}
-          <h5>{item.price}</h5> {/* Wrapped the variable in curly braces */}
-          <h5>{item.qtyOnHand}</h5> {/* Wrapped the variable in curly braces */}
-          <h5>________________________________</h5>
+        <div key={item.itemCode}>
+
+          <div className='itemDiv'>
+          <div className='imageDiv'>
+          <img src={item.imageUrl} alt="itemImage" className='itemImage'/>
+          </div> 
+          
+          <div className='desDiv'>
+          <h5>Item-Code : </h5><h6>{item.itemCode}</h6><br/><br/> 
+          <h5>Item-Name : </h5><h6>{item.itemName}</h6><br/><br/>  
+          <h5>Unit-Price : </h5><h6>{item.price}</h6><br/><br/>
+          <h5>Desctiption : </h5><h6>This Item sample description</h6><br/><br/>  
+          </div>
+
+          <button type="button" id='addToCart'><a href='/itemPage' style={{textDecoration: 'none', color: 'black'}}><h3>Add To Cart</h3></a></button><br/><br/>
+          <button type="button" id='buyBtn'><a href='/itemPage' style={{textDecoration: 'none', color: 'black'}}><h3>Buy</h3></a></button>
+
+          </div>
+       <br/>
+          
         </div>
       ))}
 
