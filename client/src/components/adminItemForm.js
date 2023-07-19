278a0827bfad251cdf726ffb99d845e1583992e6
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-07-20 00:35:19 +0530

    load data to inputs when click om item btn

--- client/src/components/adminItemForm.js
+++ client/src/components/adminItemForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './../css/adminItem.css';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar';
 
@@ -12,7 +12,12 @@ export default class adminItemForm extends Component {
         super(props);
     
         this.state = {
-          itemList : []          
+          itemList : [] ,
+          selectedItem: null,
+          itemCode: "",
+          itemName: "",
+          price: "",
+          qtyOnHand: ""         
         }
       }
 
@@ -20,14 +25,23 @@ export default class adminItemForm extends Component {
         this.loadAllItems();
       }
 
+
       loadAllItems(){
-        
         axios.get("http://localhost:8000/item/getItem").then(function (res) {
         this.setState({
         itemList: res.data
         });
         }.bind(this));
+      }
 
+      handleItemClick(item) {
+        this.setState({
+          selectedItem: item,
+          itemCode: item.itemCode,
+          itemName: item.itemName,
+          price: item.price,
+          qtyOnHand: item.qtyOnHand
+        });
       }
  
     render() {
@@ -37,6 +51,9 @@ export default class adminItemForm extends Component {
         <Routes>
         <Route path='/' exact Component={NavBar}></Route>
         </Routes>
+
+        <br/>
+        <h2><b>Manage ITEMS</b></h2>
 
         <div className='itemConDiv'>
 
@@ -57,7 +74,7 @@ export default class adminItemForm extends Component {
                     <td>{ittList.itemName}</td>
                     <td>{ittList.price}</td>
                     <td>{ittList.qtyOnHand}</td>
-                    <td><button className='delBtn'><FontAwesomeIcon icon={faDeleteLeft} /></button></td>
+                    <td><button className='delBtn' onClick={() => this.handleItemClick(ittList)} ><FontAwesomeIcon icon={faPenToSquare} /></button></td>
                   </tr>
                   </tbody>
                   ))}
@@ -71,13 +88,17 @@ export default class adminItemForm extends Component {
         <div className='editForm'>
 
         <br/><br/> 
-            <label className='lblOne'><h4><b>Item Code : </b></h4></label><input placeholder='I000' type='text'></input>
+            <label className='lblOne'><h4><b>Item Code : </b></h4></label><input placeholder='I000' type='text' value={this.state.itemCode}
+            onChange={(e) => this.setState({ itemCode: e.target.value })} ></input>
             <br/><br/>
-            <label className='lblTwo'><h4><b>Item Name : </b></h4></label><input placeholder='Sample' type='text'></input>
+            <label className='lblTwo'><h4><b>Item Name : </b></h4></label><input placeholder='Sample' type='text' value={this.state.itemName}
+            onChange={(e) => this.setState({ itemName: e.target.value })} ></input>
             <br/><br/>
-            <label className='lblThree'><h4><b>Unit Price : </b></h4></label><input placeholder='Rs.000' type='text'></input>
+            <label className='lblThree'><h4><b>Unit Price : </b></h4></label><input placeholder='Rs.000' type='text' value={this.state.price}
+            onChange={(e) => this.setState({ price: e.target.value })} ></input>
             <br/><br/>
-            <label className='lblFour'><h4><b>Qty On Hand : </b></h4></label><input placeholder='000' type='text'></input>
+            <label className='lblFour'><h4><b>Qty On Hand : </b></h4></label><input placeholder='000' type='text' value={this.state.qtyOnHand}
+            onChange={(e) => this.setState({ qtyOnHand: e.target.value })} ></input>
             <br/><br/><br/>
 
             <button className='addBtn'><h6><b>Create New</b></h6></button>
