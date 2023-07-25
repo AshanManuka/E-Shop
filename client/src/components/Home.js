276fd17f33caebed8ef0290f5b560169d8fe5cfd
ashan_manuka <manukajayarathne.coma@gmail.com>
2023-07-25 23:51:44 +0530

    chenage styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -34,7 +34,29 @@ export default class Home extends Component {
 
 
         <button type="button" id='goBtn'><a href='/itemPage' style={{textDecoration: 'none', color: 'black'}}><h3>Let's Shopping</h3></a></button>
+        <br/><br/><br/><br/>
 
+        <div className='aboutSec'>
+
+        <h1><b>we Are</b></h1>
+        <br/><br/><br/>
+        <h6><b>Nowadays, many people are getting into the IT field. So as a Trainee Software engineer, I feel the need to write "The Importance of Industrial Training".
+        The unfortunate thing is that when most people start pursuing a degree or diploma on their own, they focus only on the content of the program. It is a mistake that happens due to a lack of understanding. Because, when we choose a degree program, we need to be aware not only of the number of modules taught but also of whether those taught can be put to practical use and whether it will be possible to get job training. 
+        As an Example : We come across some concepts, and no matter how much we learn about them, we cannot understand them properly until we apply them in practice.</b></h6>
+        <br/>
+        <h6><b>Nowadays, one can see a large number of jobs in the IT field, so people can choose a job of their choice. But it is not an easy thing.
+        However, if you want to be a geek person, 
+	        *Must be able to identify and adapt to rapidly changing/new technologies
+	        *They should have the ability and knowledge to apply what they learn practically
+	        *Must be able to project collaboratively with a team
+        According to me, the best way to get all the above is "Industrial Training". These days I getting that experience so I have an understanding of it.
+        as I understand, No matter how many things we learn in 2 or 3 or 4 years in academics, we see many differences in those things in practical application. I think this is the main thing we need to adjust in the industry placement. Because this is where we take the first step into the business IT field.
+        We hope to be an employee, a freelancer, or an owner of a company, but the foundation for all of them is the chapter called "industrial training" mentioned above. So our career path is determined by what we do during that time</b></h6>
+        <br/>
+        <h6><b>-So, we know little about the field. We have a lot to learn. Learn as much as you can.-</b></h6>
+
+
+        </div>
 
     </div>
 
